a0b2cedd266a12e72648655d3034f1a528a16e12
harpreet780 <harpreetsaini276@gmail.com>
2022-05-10 15:17:30 +0530

    Fix:cors error

--- pages/api/socket.js
+++ pages/api/socket.js
@@ -2,7 +2,7 @@ import io from 'socket.io-client';
 let socket;
 
 export const SocketApi = () => {
-  socket = io('http://mobe-backend.herokuapp.com');
+  socket = io('https://mobe-backend.herokuapp.com');
   socket.connect();
   return socket;
 }
